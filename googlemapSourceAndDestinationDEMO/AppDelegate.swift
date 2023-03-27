8227dddc5e8c668e5768d42e09ad9424f40fb1b8
PATELCHIRAG4597 <124140359+PATELCHIRAG4597@users.noreply.github.com>
2023-03-27 11:21:41 +0530

    when user enter address in sourceAddTexfield.   and user enter destination in destinationAddTexfield.   and user click findbtn buttone then user get latitude and longitude data  in sourceAddLable and destinationAddlable

--- googlemapSourceAndDestinationDEMO/AppDelegate.swift
+++ googlemapSourceAndDestinationDEMO/AppDelegate.swift
@@ -6,13 +6,15 @@
 //
 
 import UIKit
-
+import GoogleMaps
+import GooglePlaces
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GMSServices.provideAPIKey("AIzaSyBQbsdN-TJn958p3dDqFzD2boxE94eN6A4")
         // Override point for customization after application launch.
         return true
     }
--- googlemapSourceAndDestinationDEMO/ViewController.swift
+++ googlemapSourceAndDestinationDEMO/ViewController.swift
@@ -3,17 +3,53 @@
 //  googlemapSourceAndDestinationDEMO
 //
 //  Created by CodeInfoWay CodeInfoWay on 3/25/23.
-//
-
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sourceAddTxt: UITextField!
+    @IBOutlet weak var destinationAddTxt: UITextField!
+    @IBOutlet weak var findbtn: UIButton!
+    @IBOutlet weak var sourceAddLbl: UILabel!
+    @IBOutlet weak var destinationAddlbl: UILabel!
+    @IBOutlet weak var drawRoutebtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func findbtnTapped(_ sender: UIButton) {
+        guard let sourceAddress = sourceAddTxt.text, let destinationAddress = destinationAddTxt.text else {
+            return
+        }
+        
+        getLocation(from: sourceAddress) { (sourceLocation) in
+            if let sourceLocation = sourceLocation {
+                self.sourceAddLbl.text = "Latitude: \(sourceLocation.latitude), Longitude: \(sourceLocation.longitude)"
+            } else {
+                self.sourceAddLbl.text = "Location not found"
+            }
+        }
+        getLocation(from: destinationAddress) { (destinationLocation) in
+            if let destinationLocation = destinationLocation {
+                self.destinationAddlbl.text = "Latitude: \(destinationLocation.latitude), Longitude: \(destinationLocation.longitude)"
+            } else {
+                self.destinationAddlbl.text = "Location not found"
+            }
+        }
+    }
+    
+    func getLocation(from address: String, completion: @escaping (_ location: CLLocationCoordinate2D?)-> Void) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            guard let placemarks = placemarks,
+            let location = placemarks.first?.location?.coordinate else {
+                completion(nil)
+                return
+            }
+            completion(location)
+        }
+    }
 }
-
